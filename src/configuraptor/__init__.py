d57b6df10e85693729ffb87f4ec36a7a932e1a3e
Robin van der Noord <robinvandernoord@gmail.com>
2023-06-14 16:15:30 +0200

    fix(lib): exposed wrong method

--- src/configuraptor/__init__.py
+++ src/configuraptor/__init__.py
@@ -8,7 +8,7 @@ Exposes TypedConfig and load_into for this library.
 from .cls import TypedConfig  # noqa: F401 imported for library reasons
 from .core import (  # noqa: F401 imported for library reasons
     all_annotations,
-    check_type,
+    convert_config,
     ensure_types,
     load_into,
 )
